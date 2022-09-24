43d3d3d3d8a1ab9e7db7d10e47ed6ba9cef3e935
Panthaweekan_Somngam <tatanoobza1234@hotmail.com>
2022-09-24 14:42:50 +0700

    Finish

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,34 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const id = req.query.roomId;
+    const result01 = rooms.findIndex((x) => x.roomId === id);
+    if (result01 === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
+    return res.json({
+      ok: true,
+      messages: rooms[result01].messages,
+    });
   } else if (req.method === "POST") {
     const rooms = readDB();
-
-    //read request body
+    const id = req.query.roomId;
+    const result01 = rooms.findIndex((x) => x.roomId === id);
+    if (result01 === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
     const text = req.body.text;
-
+    if (typeof text !== "string") {
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
+    }
+    //read request body
     //create new id
     const newId = uuidv4();
+    rooms[result01].messages.push({ messageId: newId, text: text });
+    writeDB(rooms);
+    return res.json({
+      ok: true,
+      message: rooms[result01].messages[rooms[result01].messages.length - 1],
+    });
   }
 }
