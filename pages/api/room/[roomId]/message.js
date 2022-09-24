3b0eae222e0cb1d35171070b96c911495067b3b0
Panthaweekan_Somngam <tatanoobza1234@hotmail.com>
2022-09-24 14:55:40 +0700

    Update

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,34 +4,30 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
-    const id = req.query.roomId;
-    const result01 = rooms.findIndex((x) => x.roomId === id);
-    if (result01 === -1) {
+    const roomId = req.query.roomId;
+    const roomIdIDX = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdIDX === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-    }
-    return res.json({
-      ok: true,
-      messages: rooms[result01].messages,
-    });
+    const message = rooms[roomIdIDX].messages;
+    return res.json({ ok: true, messages: message });
   } else if (req.method === "POST") {
-    const rooms = readDB();
-    const id = req.query.roomId;
-    const result01 = rooms.findIndex((x) => x.roomId === id);
-    if (result01 === -1) {
+    const rooms1 = readDB();
+    const roomId = req.query.roomId;
+    const roomIdIDX = rooms1.findIndex((x) => x.roomId === roomId);
+    if (roomIdIDX === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-    }
-    const text = req.body.text;
-    if (typeof text !== "string") {
-      return res.status(400).json({ ok: false, message: "Invalid text input" });
-    }
     //read request body
+    const text = req.body.text;
+    if (typeof text !== "string")
+      return res.status(400).json({ ok: false, message: "Invalid text input" });
     //create new id
     const newId = uuidv4();
-    rooms[result01].messages.push({ messageId: newId, text: text });
-    writeDB(rooms);
-    return res.json({
-      ok: true,
-      message: rooms[result01].messages[rooms[result01].messages.length - 1],
-    });
+    const newMes = {
+      messageId: newId,
+      text: text,
+    };
+    rooms1[roomIdIDX].messages.push(newMes);
+    writeDB(rooms1);
+    return res.json({ ok: true, message: newMes });
   }
 }
--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -1,24 +1,19 @@
 import { writeDB, readDB } from "../../../../../backendLibs/dbLib";
 
 export default function roomIdMessageIdRoute(req, res) {
-  const roomId = req.query.roomId;
-  const messageId = req.query.messageId;
-
   if (req.method === "DELETE") {
     const rooms = readDB();
-    const roomIdx = rooms.findIndex((x) => x.roomId === roomId);
-
-    if (roomIdx === -1)
+    //read value from URL
+    const roomId = req.query.roomId;
+    const messageId = req.query.messageId;
+    const roomIdIDX = rooms.findIndex((x) => x.roomId === roomId);
+    if (roomIdIDX === -1)
       return res.status(404).json({ ok: false, message: "Invalid room id" });
-
-    const messageIdx = rooms[roomIdx].messages.findIndex(
-      (x) => x.messageId === messageId
-    );
-
-    if (messageIdx === -1)
+    const mes = rooms[roomIdIDX].messages;
+    const mesIdIDX = mes.findIndex((x) => x.messageId === messageId);
+    if (mesIdIDX === -1)
       return res.status(404).json({ ok: false, message: "Invalid message id" });
-
-    rooms[roomIdx].messages.splice(messageIdx, 1);
+    mes.splice(mesIdIDX, 1);
     writeDB(rooms);
     return res.json({ ok: true });
   }
